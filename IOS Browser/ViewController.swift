82b8d2bff219df0f323041fbdc5f19b1cb64b47e
Jindal Patel <jindalpatel26@gmail.com>
2020-09-03 14:10:57 +0530

    Initial Commit

---
+++ IOS Browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Browser
+//
+//  Created by Zindal on 03/09/20.
+//  Copyright © 2020 zindal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
